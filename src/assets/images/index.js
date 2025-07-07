c5c9d77d15cb29d4caa48c7ff096b7f904f5fe4b
Ayman Nabgouri <nabgouri9@gmail.com>
2025-07-07 13:12:51 +0100

    fafa

---
+++ src/assets/images/index.js
@@ -0,0 +1,105 @@
+// Import all waffle images
+import waffleThumbnail from "./image-waffle-thumbnail.jpg";
+import waffleMobile from "./image-waffle-mobile.jpg";
+import waffleTablet from "./image-waffle-tablet.jpg";
+import waffleDesktop from "./image-waffle-desktop.jpg";
+
+// Import all creme brulee images
+import cremeBruleeThumbnail from "./image-creme-brulee-thumbnail.jpg";
+import cremeBruleeMobile from "./image-creme-brulee-mobile.jpg";
+import cremeBruleeeTablet from "./image-creme-brulee-tablet.jpg";
+import cremeBruleeDesktop from "./image-creme-brulee-desktop.jpg";
+
+// Import all macaron images
+import macaronThumbnail from "./image-macaron-thumbnail.jpg";
+import macaronMobile from "./image-macaron-mobile.jpg";
+import macaronTablet from "./image-macaron-tablet.jpg";
+import macaronDesktop from "./image-macaron-desktop.jpg";
+
+// Import all tiramisu images
+import tiramisuThumbnail from "./image-tiramisu-thumbnail.jpg";
+import tiramisuMobile from "./image-tiramisu-mobile.jpg";
+import tiramisuTablet from "./image-tiramisu-tablet.jpg";
+import tiramisuDesktop from "./image-tiramisu-desktop.jpg";
+
+import baklavaThumbnail from "./image-baklava-thumbnail.jpg";
+import baklavaMobile from "./image-baklava-mobile.jpg";
+import baklavaTablet from "./image-baklava-tablet.jpg";
+import baklavaDesktop from "./image-baklava-desktop.jpg";
+
+import meringueThumbnail from "./image-meringue-thumbnail.jpg";
+import meringueMobile from "./image-meringue-mobile.jpg";
+import meringueTablet from "./image-meringue-tablet.jpg";
+import meringueDesktop from "./image-meringue-desktop.jpg";
+
+import cakeThumbnail from "./image-cake-thumbnail.jpg";
+import cakeMobile from "./image-cake-mobile.jpg";
+import cakeTablet from "./image-cake-tablet.jpg";
+import cakeDesktop from "./image-cake-desktop.jpg";
+
+import brownieThumbnail from "./image-brownie-thumbnail.jpg";
+import brownieMobile from "./image-brownie-mobile.jpg";
+import brownieTablet from "./image-brownie-tablet.jpg";
+import brownieDesktop from "./image-brownie-desktop.jpg";
+
+import pannaCottaThumbnail from "./image-panna-cotta-thumbnail.jpg";
+import pannaCottaMobile from "./image-panna-cotta-mobile.jpg";
+import pannaCottaTablet from "./image-panna-cotta-tablet.jpg";
+import pannaCottaDesktop from "./image-panna-cotta-desktop.jpg";
+
+export const images = {
+  waffle: {
+    thumbnail: waffleThumbnail,
+    mobile: waffleMobile,
+    tablet: waffleTablet,
+    desktop: waffleDesktop,
+  },
+  cremeBrulee: {
+    thumbnail: cremeBruleeThumbnail,
+    mobile: cremeBruleeMobile,
+    tablet: cremeBruleeeTablet,
+    desktop: cremeBruleeDesktop,
+  },
+  macaron: {
+    thumbnail: macaronThumbnail,
+    mobile: macaronMobile,
+    tablet: macaronTablet,
+    desktop: macaronDesktop,
+  },
+  tiramisu: {
+    thumbnail: tiramisuThumbnail,
+    mobile: tiramisuMobile,
+    tablet: tiramisuTablet,
+    desktop: tiramisuDesktop,
+  },
+  baklava: {
+    thumbnail: baklavaThumbnail,
+    mobile: baklavaMobile,
+    tablet: baklavaTablet,
+    desktop: baklavaDesktop,
+  },
+  meringue: {
+    thumbnail: meringueThumbnail,
+    mobile: meringueMobile,
+    tablet: meringueTablet,
+    desktop: meringueDesktop,
+  },
+  cake: {
+    thumbnail: cakeThumbnail,
+    mobile: cakeMobile,
+    tablet: cakeTablet,
+    desktop: cakeDesktop,
+  },
+  brownie: {
+    thumbnail: brownieThumbnail,
+    mobile: brownieMobile,
+    tablet: brownieTablet,
+    desktop: brownieDesktop,
+  },
+  pannaCotta: {
+    thumbnail: pannaCottaThumbnail,
+    mobile: pannaCottaMobile,
+    tablet: pannaCottaTablet,
+    desktop: pannaCottaDesktop,
+  },
+};
